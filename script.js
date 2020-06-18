db8ef793468fff7d83704dda64dcabedbdb9b5b0
Juan-Rivera-Alonso <60264200+Juan-Rivera-Alonso@users.noreply.github.com>
2020-06-17 20:31:16 -0600

    calculadora protoripo 1

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function limpiar(){
+    document.getElementById("calculadora").reset();
+}
+
+function sumar(){
+    let x = parseFloat(document.getElementById("valor1").value);
+    let y = parseFloat(document.getElementById("valor2").value);
+    document.getElementById("el-resultado").innerHTML = x+y;
+}
+
+function restar(){
+    let x = parseFloat(document.getElementById("valor1").value);
+    let y = parseFloat(document.getElementById("valor2").value);
+    document.getElementById("el-resultado").innerHTML = x-y;
+}
+
+function multiplicar(){
+    let x = parseFloat(document.getElementById("valor1").value);
+    let y = parseFloat(document.getElementById("valor2").value);
+    document.getElementById("el-resultado").innerHTML = x*y;
+}
+
+function dividir(){
+    let x = parseFloat(document.getElementById("valor1").value);
+    let y = parseFloat(document.getElementById("valor2").value);
+    document.getElementById("el-resultado").innerHTML = x/y;
+}
